37aca00acff0e40ade911e2e0b69a400c4802023
Евгений Емельянов <95439615+evg9n@users.noreply.github.com>
2024-06-08 12:48:33 +0300

    Initial commit

---
+++ constants.py
@@ -0,0 +1,30 @@
+from os import environ, path, listdir, getenv
+
+from dotenv import load_dotenv
+
+
+class Constants:
+
+    def __init__(self):
+        load_dotenv(path.abspath(path.join('env', '.env')))
+        path_env = path.abspath('env')
+        try:
+            for env in listdir(path_env):
+                if env.endswith('.env'):
+                    load_dotenv(path.join(path_env, env))
+        except FileNotFoundError:
+            pass
+
+        # логирование
+        self.FORMAT_LOGGER = getenv('FORMAT_LOGGER',
+                                    default='{time:YYYY-MM-DD HH:mm:ss} | {level} | {file} | {message}')
+        self.LEVEL_FILE_LOGGER = getenv('LEVEL_FILE_LOGGER', default='DEBUG')
+        self.LEVEL_CONSOLE_LOGGER = getenv('LEVEL_CONSOLE_LOGGER', default='INFO')
+        self.ROTATION_LOGGER = getenv('ROTATION_LOGGER', default='1 day')
+        self.SERIALIZE_LOGGER = getenv('SERIALIZE_LOGGER', default=None) == 'True'
+
+    def __setattr__(self, name, value):
+        if name in self.__dict__:
+            raise AttributeError('Constants are not changeable!')
+        else:
+            super().__setattr__(name, value)
